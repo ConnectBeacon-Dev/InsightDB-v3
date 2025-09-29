1f077bda071eac0903966375ec7ce8cf088936e8
prabinapattnaik <68896015+prabinapattnaik@users.noreply.github.com>
2025-09-29 12:00:43 +0530

    Update run_queries.ps1

--- run_queries.ps1
+++ run_queries.ps1
@@ -2,14 +2,9 @@
 $DB_PATH = ".\db"
 $SCRIPT = "json_rag_win.py"
 
-# List of models
-$models = @(
-    ".\models\qwen2.5-3b-instruct-q8_0.gguf"
-    ".\models\Qwen2.5-7B-Instruct-Q4_K_M.gguf"
-	"D:\InsightDB-v2-main\models\Qwen2.5-14B-Instruct-Q4_K_M.gguf"
-)
-# Single log file
-$logFile = ".\output_all.log"
+# model Path
+$MODEL_PATH = ".\models\qwen2.5-3b-instruct-q8_0.gguf"
+
 
 # List of queries
 $queries = @(
@@ -26,34 +21,15 @@ $queries = @(
   "MSME Companies"
   "How many products supplied to HAL in Gujarat"
   "List companies having ISO 9001 certificate"
-  
 )
 
 # Loop through queries
-# Loop through models and queries
-
-# Start fresh
-"==== Starting Run $(Get-Date) ====" | Out-File -FilePath $logFile
-
-# Loop through models and queries
-foreach ($model in $models) {
-    "##############################################" | Out-File -FilePath $logFile -Append
-    "Running queries with model: $model" | Out-File -FilePath $logFile -Append
-    "##############################################" | Out-File -FilePath $logFile -Append
-
-    foreach ($q in $queries) {
-        "==============================================" | Out-File -FilePath $logFile -Append
-        (Get-Date) | Out-File -FilePath $logFile -Append
-        "Running query: $q" | Out-File -FilePath $logFile -Append
-
-        # Run query and append both stdout and stderr to log file
-        python $SCRIPT query --db $DB_PATH --model $model --ask "$q" *>> $logFile
-
-        (Get-Date) | Out-File -FilePath $logFile -Append
-        "==============================================" | Out-File -FilePath $logFile -Append
-        "" | Out-File -FilePath $logFile -Append
-    }
+foreach ($q in $queries) {
+    Write-Output "=============================================="
+    Get-Date
+    Write-Output "Running query: $q"
+    python $SCRIPT query --db $DB_PATH --model $MODEL_PATH --ask "$q"
+    Get-Date
+    Write-Output "=============================================="
+    Write-Output ""
 }
-
-"==== Run Completed $(Get-Date) ====" | Out-File -FilePath $logFile -Append
-
